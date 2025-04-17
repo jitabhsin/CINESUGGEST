3fd7da9d79113c601a067b4376f7c90a60132c7f
jitabhsin <jitabhsin@gmail.com>
2025-04-17 22:39:03 +0530

    Initial commit of CineSuggest Spring Boot project

---
+++ src/main/java/com/cinesuggest/CineSuggestApplication.java
@@ -0,0 +1,13 @@
+package com.cinesuggest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineSuggestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineSuggestApplication.class, args);
+	}
+
+}
